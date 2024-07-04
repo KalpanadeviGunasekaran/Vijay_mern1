80407829dc61023594291ee3c96270d78aff9c54
poorna <kalpanadevig.21cse@kongu.edu>
2024-07-04 17:41:01 +0530

    first

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import Logo from './Logo'
+// import Logo from './Logo'
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
@@ -55,11 +55,16 @@ const Header = () => {
   return (
     <header className='h-16 shadow-md bg-white fixed w-full z-40'>
       <div className=' h-full container mx-auto flex items-center px-4 justify-between'>
-            <div className=''>
-                <Link to={"/"}>
-                    <Logo w={90} h={50}/>
-                </Link>
-            </div>
+      <div>
+          <a href="/" className="flex items-center">
+            <img
+              src="https://tse1.mm.bing.net/th?id=OIP.Y3ZpDSb70FtyQBAwyg2naQHaHa&pid=Api&P=0&h=180"
+              alt="Logo"
+              height="30"
+              width="60"
+            />
+          </a>
+        </div>
 
             <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
                 <input type='text' placeholder='search product here...' className='w-full outline-none' onChange={handleSearch} value={search}/>
